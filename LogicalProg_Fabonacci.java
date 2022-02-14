dd821cfa0303cc8b9a29a9085879c00a15b5f321
priyagadhave <96239000+priyagadhave@users.noreply.github.com>
2022-02-14 20:23:27 +0530

    Add files via upload

---
+++ LogicalProg_Fabonacci.java
@@ -0,0 +1,29 @@
+package day6assignments;
+import java.util.Scanner;
+
+public class LogicalProg_Fabonacci {
+
+
+	
+	public static void getSeries() {
+			Scanner sc=new Scanner(System.in);
+			System.out.println("Please enter the limit until you want the Fiboncci Series :");
+			int limit=sc.nextInt();
+			int series[]=new int[limit];
+			series[0]=0;
+			series[1]=1;
+			for (int i=2;i<series.length;i++)
+			{
+				series[i]=series[i-1]+series[i-2];
+				
+			}
+			for(int j:series)            //for each loop to display array elements
+			{
+				System.out.print(j+" ");
+			}
+			sc.close();
+		}
+		public static void main(String[] args) {
+			getSeries();
+		}
+	}
---
+++ ToBinary.java
@@ -0,0 +1,30 @@
+package JUnitTesting;
+
+import java.util.Scanner;
+
+public class ToBinary {
+
+
+		public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+	        System.out.print("Enter a Number To Find The Square Root : ");
+	        int inputNumber = scanner.nextInt();
+	        scanner.close();
+	        final double epsilon = 1E-15;
+	        float t = inputNumber;
+	       
+	        while (Math.abs(t-inputNumber/t) > epsilon * t) {
+	            double lastT = t;
+	            t = (float) ((inputNumber/t + t)/2.0);
+	            if ( lastT == t) {
+	                break;
+	            }
+	        }
+	        System.out.println("Square Root of " + inputNumber + " : " + t);
+	    }
+	
+		// TODO Auto-generated method stub
+
+	
+
+}
